d2f357b7798751efa2e6e52e16229ab72a876007
inush <inushongg@gmail.com>
2023-06-26 02:40:44 +0900

    변경

--- app/src/main/java/com/inu/inujeungplayer/repository/db/RoomMusicHelper.kt
+++ app/src/main/java/com/inu/inujeungplayer/repository/db/RoomMusicHelper.kt
@@ -6,7 +6,7 @@ import com.inu.inujeungplayer.repository.dao.RoomMusicDao
 import com.inu.inujeungplayer.model.Music
 import com.inu.inujeungplayer.model.Radio
 
-@Database(entities = [Music::class, Radio::class], version = 1)
+@Database(entities = [Music::class, Radio::class], version = 1, exportSchema = false)
 abstract class RoomMusicHelper: RoomDatabase() {
     abstract fun roomMusicDao(): RoomMusicDao
 }
